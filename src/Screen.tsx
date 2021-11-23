abf522828214c105f159eed1d8a1069ff8e214e7
zaki98D <ounissizakaria@gmail.com>
2021-11-23 17:16:52 +0100

    edit screen inputs/style & add ans placeholder

--- src/Screen.tsx
+++ src/Screen.tsx
@@ -1,13 +1,18 @@
 
 interface ScreenParams {
-  children: string;
   className?: string;
+  equation: string;
+  ans: number;
+  showResult: boolean;
 }
-export function Screen({ children, className }: ScreenParams) {
+export function Screen({ className, equation, ans, showResult }: ScreenParams) {
   return (
     <div className={"relative bg-gray-100 " + className}>
-      <p className="absolute font-mono text-3xl font-bold tracking-widest right-4 bottom-4">
-        {children}
+      <p className={`absolute font-mono font-bold tracking-widest transition-all right-4 ${showResult ? "text-lg bottom-16" : "text-3xl bottom-4"}`} >
+        {equation}
+      </p>
+      <p className={`absolute font-mono font-bold tracking-widest text-3xl bottom-4 right-4 ${showResult ? "transition-all visible" : "invisible"}`} >
+        {ans}
       </p>
     </div>
   );
