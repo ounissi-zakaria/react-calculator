a8c1b258699e03b9896624db35a5920dc951b057
zaki98D <ounissizakaria@gmail.com>
2021-11-23 15:08:07 +0100

    fix operation duplecation bug

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,13 @@ function App() {
       <Screen className="row-span-5 col-span-full">{state.join("")}</Screen>
       {buttons.map(element => {
         let write = ({ state, setState }: actionParams) => {
-          return () => setState([...state, element.value])
+          let newState = [...state]
+          let last = newState[newState.length - 1]
+          if (/\d/.test(last) && element.type === "number") newState[newState.length - 1] = last + element.value
+          else if (element.type === "number") newState.push(element.value)
+          else if (/\d/.test(last) && element.type === "operation") newState.push(element.value)
+          else if (/[\*\/\-\+]/.test(last) && element.type === "operation") newState[newState.length - 1] = element.value
+          return () => setState(newState)
         }
         let action = element.action ?? write
         return <Button className={element.className} onClick={action({ state, setState })} key={element.value}>{element.value}</Button>
@@ -30,6 +36,7 @@ export default App
 const buttons = [
   {
     value: "AC",
+    type: "function",
     action: ({ state, setState }: actionParams) => {
       let closure = () => setState(Array())
       return closure
@@ -37,66 +44,85 @@ const buttons = [
   },
   {
     value: "C",
+    type: "function",
     action: ({ state, setState }: actionParams) => {
       let closure = () => {
-        state[state.length - 1] = state[state.length - 1]?.slice(0, -1)
-        setState(state)
+        let newState = [...state]
+        newState[newState.length - 1] = newState[newState.length - 1]?.slice(0, -1)
+        if (newState[newState.length - 1] === "") newState.pop()
+        setState(newState)
       }
       return closure
     }
   },
   {
     value: "/",
+    type: "operation",
   },
   {
     value: "*",
+    type: "operation",
   },
   {
     value: "7",
+    type: "number",
   },
   {
     value: "8",
+    type: "number",
   },
   {
     value: "9",
+    type: "number",
   },
   {
     value: "-",
+    type: "operation",
   },
   {
     value: "4",
+    type: "number",
   },
   {
     value: "5",
+    type: "number",
   },
   {
     value: "6",
+    type: "number",
   },
   {
     value: "+",
+    type: "operation",
   },
   {
     value: "1",
+    type: "number",
   },
   {
     value: "2",
+    type: "number",
   },
   {
     value: "3",
+    type: "number",
   },
   {
     value: "=",
+    type: "function",
     className: "row-span-2",
     action: ({ state, setState }: actionParams) => {
-      let closure = () => setState([eval(state.join(""))])
+      let closure = () => setState([String(eval(state.join("")))])
       return closure
     }
   },
   {
     value: "0",
     className: "col-span-2",
+    type: "number",
   },
   {
     value: ".",
+    type: "operation",
   },
 ]
--- src/Screen.tsx
+++ src/Screen.tsx
@@ -6,7 +6,7 @@ interface ScreenParams {
 export function Screen({ children, className }: ScreenParams) {
   return (
     <div className={"relative bg-gray-100 " + className}>
-      <p className="absolute font-mono text-xl font-bold tracking-widest right-4 bottom-4">
+      <p className="absolute font-mono text-3xl font-bold tracking-widest right-4 bottom-4">
         {children}
       </p>
     </div>
